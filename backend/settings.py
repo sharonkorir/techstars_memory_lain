08fd9ca1ed73d4f88a9d29b3fdcdf31b4194b96f
sharonkorir <csharonkorir@gmail.com>
2023-07-09 02:42:46 +0300

    configure database

--- backend/settings.py
+++ backend/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
